bfe23123d08c97d670016644f7c8fcc8bb104b9b
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2021-09-15 23:18:42 +0900

    viewのヘッダー部分完成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <p class="soc">S.O.C Fishing</p>
+        <div class="nav">
+            <a href="">釣り場一覧</a>
+            <span>|</span>
+            <a href="">ターゲット紹介</a>
+        </div>
+    </header>
+    <div class="mainImg">
+        <div class="title">
+            <h1>
+                関東釣り場
+                <br>
+                （サーフ）
+            </h1>
+            <div class="link">
+                <a href="">S.O.Cのインスタはこちら</a>
+                <a href="">S.O.C Fishingのインスタはこちら</a>
+            </div>
+        </div>
+    </div>
+    <div>
+        <h1>釣り場一覧</h1>
+        <form action="">
+            <p>条件を絞る</p>
+            <label>都道府県<input type="text"></label>
+            　ターゲット
+            <select>
+                <option value="">選択しない</option>
+                <option value="">シロギス</option>
+                <option value="">カレイ</option>
+                <option value="">マゴチ</option>
+                <option value="">ヒラメ</option>
+                <option value="">イシモチ</option>
+                <option value="">イナダ</option>
+                <option value="">タチウオ</option>
+                <option value="">シーバス</option>
+            </select>
+
+        </form>
+        <table>
+            <thead>
+                <tr>
+                    <th>釣り場名</th>
+                    <th>都道府県</th>
+                    <th>No.1ターゲット</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>三浦海岸</td>
+                    <td>神奈川県</td>
+                    <td>シロギス</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
